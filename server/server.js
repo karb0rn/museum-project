e40bf5d9538b91fa5820f79e7537200c3fa7e194
karbon <shiva.karthick07022004@gmail.com>
2026-07-27 14:55:29 +0530

    another update.....I wanna kill myself

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
+import fs from "fs";
 import connectDB from "./config/db.js";
 import artifactRoutes from "./routes/artifactRoutes.js";
 
@@ -23,12 +24,31 @@ app.use(express.json());
 // API Routes
 app.use("/api/artifacts", artifactRoutes);
 
+const distPath = path.join(__dirname, "../client/dist");
+
+console.log("Dist path:", distPath);
+console.log("Dist exists:", fs.existsSync(distPath));
+
 // Serve React build
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
 // React Router fallback
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+// Catch unexpected errors
+process.on("uncaughtException", (err) => {
+  console.error("UNCAUGHT EXCEPTION:", err);
+});
+
+process.on("unhandledRejection", (reason) => {
+  console.error("UNHANDLED REJECTION:", reason);
 });
 
 const PORT = process.env.PORT || 5000;
